3efacc5ef4f8422155076cb61ae7a1b7bcc456b1
Rui Zhang <rui_zhang@baylor.edu>
2017-01-30 17:32:08 -0600

    Jan 30 MUX test

--- code/0_common/mux_test.v
+++ code/0_common/mux_test.v
@@ -1,7 +1,8 @@
-`timescale 1ns / 1ps
+`include "definitions.vh"
 
 module mux_test;
-
+`define halfsize  16
+ 
 	reg [15:0] Ain;
 	reg [15:0] Bin;
 	reg control;
@@ -15,11 +16,42 @@ module mux_test;
 	);
 
 	initial begin
-		Ain <= 0;
-		Bin <= 0;
+		Ain <=`WORD'b0; ;
+		Bin <= `WORD'b1;
 		control <= 0;
         #10;
-
+        Ain <= `WORD'b0;
+		Bin <= `WORD'b1;
+		control <= 1;
+        #10;
+        
+        Ain <= `WORD'b1;
+        Bin <= `WORD'b0;
+        control <= 0;
+        #10;      
+        Ain <= `WORD'b1;
+        Bin <= `WORD'b0;
+        control <= 1;
+        #10;
+                        
+        Ain <= `WORD'd11;
+        Bin <= `WORD'd111;
+        control <= 0;
+        #10;
+        Ain <= `WORD'd11;
+        Bin <= `WORD'd111;
+        control <= 1;
+        #10;
+        
+        Ain <= `WORD'd255;
+        Bin <= `WORD'd210;
+        control <= 0;
+        #10;
+        Ain <= `WORD'd255;
+        Bin <= `WORD'd210;
+        control <= 1;
+        #10;
+                
 	end
       
 endmodule
